9758751105a1a5b6d80726326f6c962c323498cc
leiqichn <lei_qi@outlook.com>
2024-01-23 23:09:46 +0800

    upload 20240123

---
+++ 20240114/83-remove-duplicates-from-sorted-list.go
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2024 Lei Qi. All rights reserved.
+ * Author: Lei Qi
+ * Description:
+ * Date: 2024/1/14 下午5:51
+ */
+
+package _0240114
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	for i := head; i != nil && i.Next != nil; i = i.Next {
+		if i.Next.Val == i.Val {
+			i.Next = i.Next.Next
+		} else {
+			i = i.Next
+		}
+	}
+	return head
+}
