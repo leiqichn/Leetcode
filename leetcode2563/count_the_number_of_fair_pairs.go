145fab25877aaba8f13a31636a9c6adc13e99690
Lei Qi <lei_qi@outlook.com>
2025-04-19 10:58:07 +0800

    feat: 遍历j,二分找i的方法，更简便

--- leetcode2563/count_the_number_of_fair_pairs.go
+++ leetcode2563/count_the_number_of_fair_pairs.go
@@ -1,37 +1,17 @@
 package leetcode2563
 
-import "sort"
+import (
+	"slices"
+	"sort"
+)
 
-func countFairPairs(nums []int, lower int, upper int) int64 {
-	sort.Ints(nums)
-	var count int64
-	n := len(nums)
-	for i := 0; i < n; i++ {
-		lowVal := lower - nums[i]
-		highVal := upper - nums[i]
-		start := i + 1
-		end := n - 1
-		if start > end {
-			continue
-		}
-		subStart := start
-		subLength := end - subStart + 1
-
-		// 查找第一个大于等于lowVal的位置
-		leftInSub := sort.Search(subLength, func(k int) bool {
-			return nums[subStart+k] >= lowVal
-		})
-		left := subStart + leftInSub
-
-		// 查找第一个大于highVal的位置，然后减一得到最后一个<=highVal的位置
-		rightInSub := sort.Search(subLength, func(k int) bool {
-			return nums[subStart+k] > highVal
-		}) - 1
-		right := subStart + rightInSub
-
-		if left <= right && right >= subStart {
-			count += int64(right - left + 1)
-		}
+func countFairPairs(nums []int, lower, upper int) (ans int64) {
+	slices.Sort(nums)
+	for j, x := range nums {
+		// 注意要在 [0, j-1] 中二分，因为题目要求两个下标 i < j
+		r := sort.SearchInts(nums[:j], upper-x+1)
+		l := sort.SearchInts(nums[:j], lower-x)
+		ans += int64(r - l)
 	}
-	return count
+	return
 }
