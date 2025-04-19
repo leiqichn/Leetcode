348c56ddda5071fffbf40a136e97563352bc379c
Lei Qi <lei_qi@outlook.com>
2025-04-19 10:46:01 +0800

    feat: 遍历i,二分找j的方法

--- leetcode2563/count_the_number_of_fair_pairs.go
+++ leetcode2563/count_the_number_of_fair_pairs.go
@@ -3,24 +3,35 @@ package leetcode2563
 import "sort"
 
 func countFairPairs(nums []int, lower int, upper int) int64 {
-	// 两层遍历
-	count := 0
 	sort.Ints(nums)
-	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j < len(nums); j++ {
-			// 超出时间显示 如何减枝
-			if lower > nums[i]+nums[j] {
-				continue
-			}
+	var count int64
+	n := len(nums)
+	for i := 0; i < n; i++ {
+		lowVal := lower - nums[i]
+		highVal := upper - nums[i]
+		start := i + 1
+		end := n - 1
+		if start > end {
+			continue
+		}
+		subStart := start
+		subLength := end - subStart + 1
 
-			if upper < nums[i]+nums[j] {
-				break
-			}
+		// 查找第一个大于等于lowVal的位置
+		leftInSub := sort.Search(subLength, func(k int) bool {
+			return nums[subStart+k] >= lowVal
+		})
+		left := subStart + leftInSub
 
-			if lower <= nums[i]+nums[j] && upper >= nums[i]+nums[j] {
-				count++
-			}
+		// 查找第一个大于highVal的位置，然后减一得到最后一个<=highVal的位置
+		rightInSub := sort.Search(subLength, func(k int) bool {
+			return nums[subStart+k] > highVal
+		}) - 1
+		right := subStart + rightInSub
+
+		if left <= right && right >= subStart {
+			count += int64(right - left + 1)
 		}
 	}
-	return int64(count)
+	return count
 }
