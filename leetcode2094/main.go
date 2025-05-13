4c3606dee20515b3493f460551b5cc10606c81b2
Lei Qi <lei_qi@outlook.com>
2025-05-13 22:40:19 +0800

    feat: 需要使用map去重

--- leetcode2094/main.go
+++ leetcode2094/main.go
@@ -3,17 +3,30 @@ package leetcode2094
 import "sort"
 
 func findEvenNumbers(digits []int) []int {
-	//暴力
-	res := []int{}
-	for i := 0; i < len(digits); i++ {
-		for j := i + 1; j < len(digits); j++ {
-			for k := j + 1; k < len(digits); k++ {
-				if digits[i] != 0 && digits[k]%2 == 0 {
-					tmpNums := digits[i]*100 + digits[k]*10 + digits[k]
-					res = append(res, tmpNums)
+	resMap := make(map[int]bool) // 使用map 进行去重
+	n := len(digits)
+	for i := 0; i < n; i++ {
+		if digits[i] == 0 { // 首位数组不能为0
+			continue
+		}
+		for j := 0; j < n; j++ {
+			if j == i { // index 不要相同
+				continue
+			}
+			for k := 0; k < n; k++ {
+				if k == i || k == j { // index 不要相同
+					continue
+				}
+				if digits[k]%2 == 0 {
+					num := digits[i]*100 + digits[j]*10 + digits[k]
+					resMap[num] = true // map
 				}
 			}
 		}
+	}
+	res := []int{}
+	for num := range resMap {
+		res = append(res, num) // map转ints
 	}
 	sort.Ints(res)
 	return res
