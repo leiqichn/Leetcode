fcba00e09d0730ea49f6b62e7d0062321a5769db
Lei Qi <lei_qi@outlook.com>
2025-05-18 22:54:53 +0800

    feat: 增加返回值和初始化area = 1

--- leetcode695/main.go
+++ leetcode695/main.go
@@ -7,42 +7,44 @@ func maxAreaOfIsland(grid [][]int) int {
 		{0, -1},
 		{0, 1},
 	}
-	var dfs func([][]int, int, int)
-	// 淹掉陆地
-	dfs = func(grid [][]int, x, y int) {
+
+	var dfs func([][]int, int, int) int // 修改返回类型为int
+
+	dfs = func(grid [][]int, x, y int) int { // 返回当前岛屿面积
 		row := len(grid)
 		col := len(grid[0])
 
+		// 越界检查
 		if x < 0 || x >= row || y < 0 || y >= col {
-			return
+			return 0
 		}
 
-		if grid[x][y] == '0' {
-			return
+		// 遇到海水或已访问
+		if grid[x][y] == 0 {
+			return 0
 		}
 
-		grid[x][y] = '0'
+		// 淹没当前点并初始化面积为1
+		grid[x][y] = 0
+		area := 1
+
+		// 向四个方向扩散并累加面积
 		for _, d := range dic {
-			xNext := x + d[0]
-			yNext := y + d[1]
-			dfs(grid, xNext, yNext)
-
+			area += dfs(grid, x+d[0], y+d[1])
 		}
+		return area
 	}
 
-	if len(grid) == 0 {
-		return 0
-	}
-
-	count := 0
+	maxArea := 0
 	for i := 0; i < len(grid); i++ {
 		for j := 0; j < len(grid[0]); j++ {
-			if grid[i][j] == '1' {
-				dfs(grid, i, j)
-				count++
+			if grid[i][j] == 1 {
+				currentArea := dfs(grid, i, j)
+				if currentArea > maxArea {
+					maxArea = currentArea
+				}
 			}
 		}
 	}
-	return count
-
+	return maxArea
 }
