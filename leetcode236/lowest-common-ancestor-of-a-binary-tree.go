05793c1c93fc08ebe9a941bfa420152f95313e48
Lei Qi <lei_qi@outlook.com>
2025-06-02 18:00:52 +0800

    feat: 添加注释

--- leetcode236/lowest-common-ancestor-of-a-binary-tree.go
+++ leetcode236/lowest-common-ancestor-of-a-binary-tree.go
@@ -11,19 +11,24 @@ package leetcode236
 import . "lcutils/treenode"
 
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	// 当前节点为空，当前节点为p, 当前节点为q
 	if root == nil || root == p || root == q {
 		return root
 	}
 	left := lowestCommonAncestor(root.Left, p, q)
 	right := lowestCommonAncestor(root.Right, p, q)
-
+	// 其他：左右子树都找到， 返回当前节点
 	if left != nil && right != nil {
 		return root
 	}
-
+	// 只有左子树找到
 	if left != nil {
 		return left
 	}
-
-	return right
+	// 只有右子树找到
+	if right != nil {
+		return right
+	}
+	// 左右都没找到
+	return nil
 }
