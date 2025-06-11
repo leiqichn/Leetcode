8e71b87e6d5c841c60e05c4416cf26bb61196ee2
Lei Qi <lei_qi@outlook.com>
2025-06-12 00:10:39 +0800

    feat: 增加恢复和入口

--- leetcode22/22-选括号.go
+++ leetcode22/22-选括号.go
@@ -18,14 +18,17 @@ func generateParenthesis(n int) []string {
 		if open < n {
 			path = append(path, "(")
 			dfs(i+1, open+1)
+			path = path[:len(path)-1]
 		}
 
 		// 选右边
-		if i-open < n {
+		if i-open < open { // 右边要小于左边才能加
 			path = append(path, ")")
 			dfs(i+1, open)
+			path = path[:len(path)-1]
 		}
 
 	}
+	dfs(0, 0) // 入口函数需要添加
 	return res
 }
