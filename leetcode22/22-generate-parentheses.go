ac539964b03a75e110fa98ec38da66cf011198a7
Lei Qi <lei_qi@outlook.com>
2025-06-12 00:05:06 +0800

    feat: 添加注释

--- leetcode22/22-generate-parentheses.go
+++ leetcode22/22-generate-parentheses.go
@@ -5,14 +5,14 @@ func generateParenthesis(n int) (ans []string) {
 	m := n * 2
 	path := make([]byte, m)
 	var dfs func(int, int)
-	dfs = func(i, open int) {
+	dfs = func(i, open int) { // open设定为左括号的数量
 		if i == m {
 			ans = append(ans, string(path))
 			return
 		}
-		if open < n { // 可以填左括号
+		if open < n { // 可以填左括号， 左括号小于一半，则可以添加
 			path[i] = '('
-			dfs(i+1, open+1)
+			dfs(i+1, open+1) //到下一个位置，同时左括号添加1
 		}
 		if i-open < open { // 可以填右括号
 			path[i] = ')'
