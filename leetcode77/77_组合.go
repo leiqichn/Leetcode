29bfa040724d99b0cdfce9c3809444fcde2de921
Lei Qi <lei_qi@outlook.com>
2025-06-11 23:09:30 +0800

    feat: 组合修改copy, 修改减枝长度

--- leetcode77/77_组合.go
+++ leetcode77/77_组合.go
@@ -1,33 +1,32 @@
 package leetcode77
 
-func combine2(n int, k int) [][]int {
+func combine(n int, k int) [][]int {
 	// 组合
 	if k > n {
 		return [][]int{}
 	}
 	res := [][]int{}
 	path := []int{}
-	var dfs func(i int) // 下一个位置的组合
-	dfs = func(i int) {
+	var dfs func(start int) // 下一个位置的组合
+	dfs = func(start int) {
 		// base case
 		if len(path) == k {
 			tmp := make([]int, k)
-			copy(path, tmp)
-			res = append(res, path)
+			copy(tmp, path) // copy 目标值在前边
+			res = append(res, tmp)
 		}
 
-		//path 还需要 n - len(path)
-		for i := n; i >= 1; i-- {
-			if i < n-len(path) {
-				continue
+		for i := start; i >= 1; i-- {
+			//path 还需要 k - len(path) 提前减枝
+			if i < k-len(path) { // 注意是k - len(path)
+				break
 			}
+
 			path = append(path, i)
 			dfs(i - 1)
 			path = path[:len(path)-1]
 		}
 	}
-
 	dfs(n)
-
 	return res
 }
