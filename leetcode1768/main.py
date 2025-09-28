c99cb28ea94ee63d767b4451f684eebacd9a4f3e
Lei Qi <lei_qi@outlook.com>
2025-09-28 23:37:57 +0800

    feat: python 正确写法

--- leetcode1768/main.py
+++ leetcode1768/main.py
@@ -9,10 +9,11 @@ class Solution(object):
         n = len(word1)
         m = len(word2)
 
-        for i=0; i < n or i <m ;i++: # 错误 python中for 使用range
+        # 使用range循环，取两个字符串长度的最大值
+        for i in range(max(n, m)):   # for i=0; i < n or i <m ;i++: # 错误 python中for 使用range
             if i < n:
                 ans += word1[i]
-            if i < m:
+            if i < m:  # 这里修正了中文冒号
                 ans += word2[i]
 
         return ans
